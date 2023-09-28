09256250aefd1fec61ccf157ed3418be2e5f3dc3
Jordan Levy <jordan.levy@bigspark.dev>
2023-09-28 15:35:10 +0100

    reproduce content error from static host

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc>
+      <template #not-found>
+        <h1>Not Found Template</h1>
+        <p>Check the console output to see the 404.</p>
+      </template>
+    </ContentDoc>
   </main>
 </template>
